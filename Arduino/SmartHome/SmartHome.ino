64841d6b0ed0c4a25aafc389cf5d242832536e26
SabinSJ <39478000+SabinSJ@users.noreply.github.com>
2020-12-17 18:44:30 +0200

    Update BluetoothMethod

--- Arduino/SmartHome/SmartHome.ino
+++ Arduino/SmartHome/SmartHome.ino
@@ -3,23 +3,28 @@ int LED_BL = 12;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(LED_BL,OUTPUT);
+    pinMode(13,OUTPUT);
     digitalWrite(LED_BL, HIGH);
+
 }
 
 void BluetoothMethod()
 {
-        if(Serial.available() > 0)
-        {
-              incoming_value = Serial.read();
-              switch(incoming_value)
-              {
-                case '0' : digitalWrite(LED_BL,HIGH);break;
-                case '1' : digitalWrite(LED_BL,LOW);break;
-              }
-              Serial.println(incoming_value);
-        }
-        delay(50);
+      if(Serial.available() > 0)
+      {
+            incoming_value = Serial.read();
+            Serial.print(incoming_value);
+            Serial.print("\n");
+
+            if(incoming_value == '1')
+            {
+                digitalWrite(LED_BL, LOW);
+            }
+            else if(incoming_value == '0')
+            {
+                digitalWrite(LED_BL, HIGH);
+            }
+      }
 }
 
 void loop() {
